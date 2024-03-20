dccc7278cd2c3144fa7b999e354a82cb8e19158e
Dan Koskei <qimtaid@gmail.com>
2024-03-21 00:18:58 +0300

    complete

--- index.js
+++ index.js
@@ -7,3 +7,21 @@ function isRose(cat){
         return 'Sorry, please pass in a cat name'
     }
 }
+
+
+
+function isRose(cat){
+    cat === 'rose'? 'Hi Rose you are cute' : 'Hi ${cat}!'
+      
+}
+
+function isRose(cat){
+    switch(cat){
+        case 'rose':
+            return 'Hi rose you are cute!'
+            case 'ted':
+                return 'Hi ted nice to see you!'
+        default:
+            return 'Hi ${cat}!'
+    }
+}
